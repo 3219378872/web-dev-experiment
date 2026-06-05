7a49026d6f64ae3515948d9e806c00ea42f07e73
风起 <3219378872@qq.com>
2026-06-05 10:24:14 +0800

    fix(user-service): 允许 bean 定义覆盖

--- user-service/src/test/java/com/hmall/UserServiceTestBase.java
+++ user-service/src/test/java/com/hmall/UserServiceTestBase.java
@@ -15,7 +15,7 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest(properties = {
     "spring.cloud.bootstrap.enabled=false",
-    "spring.autoconfigure.exclude=org.springframework.cloud.openfeign.FeignAutoConfiguration,org.springframework.cloud.openfeign.FeignClientsConfiguration"
+    "spring.main.allow-bean-definition-overriding=true"
 })
 @Transactional
 public abstract class UserServiceTestBase {
