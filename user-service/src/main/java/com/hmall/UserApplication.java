2aaf42ab2d4bc8993708e66b29497e41bf523b6a
风起 <3219378872@qq.com>
2026-06-05 09:54:05 +0800

    fix(user-service): 修复 Feign 客户端配置冲突

--- user-service/src/main/java/com/hmall/UserApplication.java
+++ user-service/src/main/java/com/hmall/UserApplication.java
@@ -1,6 +1,7 @@
 package com.hmall;
 
 import com.hmall.api.client.ItemClient;
+import com.hmall.api.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @MapperScan({"com.hmall.mapper"})
 @SpringBootApplication
-@EnableFeignClients(basePackageClasses = {ItemClient.class})
+@EnableFeignClients(basePackageClasses = {ItemClient.class}, defaultConfiguration = {DefaultFeignConfig.class})
 class UserApplication {
 
     public static void main(String[] args) {
